4d346e855b7c2aeb475fe906de7628af8ae60678
AbinavRavi <abinav.ravi@tum.de>
2019-05-18 10:43:41 +0200

    dataloader is working finally

--- dataloader.py
+++ dataloader.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 from skimage import io
 from skimage.transform import resize
 from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils
+from torchvision import transforms
+from torchvision.transforms import Compose
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -16,25 +17,44 @@ warnings.filterwarnings("ignore")
 
 class SmokeDataset(Dataset):
     """Contains the dataloader for Smoke Simulation images generated from Mantaflow"""
-    def __init__(self, pickle,transform=None):
+    def __init__(self, data,transform=None):
         """pickle - The pickle file containing the simulated dataset of images"""
-        with open('./data/velocity64.pickle', 'rb') as handle:
-            velocities = pickle.load(handle)
-        for i in range(velocities.shape[0]):
-            velocities[i,:,:,:]=2*(velocities[i,:,:,:]-velocities[i,:,:,:].min())/(velocities[i,:,:,:].max()-velocities[i,:,:,:].min())-1
-        self.pickle = velocities
+                # for i in range(velocities.shape[0]):
+            # velocities[i,:,:,:]=(velocities[i,:,:,:]-velocities[i,:,:,:].min())/(velocities[i,:,:,:].max()-velocities[i,:,:,:].min())
+        self.velocities = np.load(data, allow_pickle=True)
         self.transform = transform
 
     def __len__(self):
-        return len(self.pickle)
+        return self.velocities.shape[0]
 
     def __getitem__(self,index):
-        img_name = os.path.join(self.root.dir,self.pickle[idx])
-        image = io.imread(img_name)
+        # img_name = os.path.join(self.root.dir,self.pickle[idx])
+        image = self.velocities[index,:,:,:]
         if self.transform:
-            image = self.transform(image)
+            resized_image = self.transform(image)
 
+        return image,resized_image
+
+class Normalize(object):
+    """Normalise the images"""
+
+    def __call__(self, image):
+        image = (image - np.min(image))/(np.max(image)-np.min(image))
         return image
+    
+    def __repr__(self):
+        return self.__class__.__name__+'()'
+
+class Resize(object):
+    """Resize the image for the generator"""
+
+    def __call__(self,image):
+        image = resize(image,(8,8),anti_aliasing=False)  
+        return image
+
+    def __repr__(self):
+        return self.__class__.__name__+'()'
+    
 
 
     
